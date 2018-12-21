310f862f9030da003995c735e0d5c2f0dfe98662
Azat Nizameev <azat.nizam@gmail.com>
2018-12-21 19:51:30 +0300

    Updating google sheets

--- public/index.php
+++ public/index.php
@@ -2,35 +2,57 @@
 require(__DIR__ . '/../vendor/autoload.php');
 $config = include(__DIR__ . "/../env/env.php");
 
-echo "<h3>Google Sheets API</h3>";
 putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/../env/' . $config["CREDENTIALS_JSON"]);
+
+// Put here your new rows
+$updValues = [
+    ["1", "1", "1", "1", "1", "1", "1"],
+    ["2", "2", "2", "2", "2", "2", "2"]
+];
 
 $client = new Google_Client();
 $client->useApplicationDefaultCredentials();
 $client->addScope("https://www.googleapis.com/auth/spreadsheets");
 
 $sheetsService = new Google_Service_Sheets($client);
+$rangeBody = new Google_Service_Sheets_ValueRange( ["values" => $updValues] );
+
+echo "<h3>Update Google Sheets via API</h3>";
 
 try {
  
-    // $response = $sheetsService->spreadsheets_values->get("1bwC2qGEJ0oU2ZLUa_kyQAEVBmlS5lybaKfJu9NDhYQo", "2 сентября!A1:C22");
-    $response = $sheetsService->spreadsheets_values->get("1DTdFIKfifM2pNiEZZ9dlemRNTBLumNm9KXXRZRTV9vY", "Выполнено!A1:C22");
+    // Calculating count of existing rows
+    $response = $sheetsService->spreadsheets_values->get("1DTdFIKfifM2pNiEZZ9dlemRNTBLumNm9KXXRZRTV9vY", "Выполнено (копия)");
+    $tableCurRowsCount = count($response["values"]);
 
-    // $response = $sheetsService->spreadsheets->get("1bwC2qGEJ0oU2ZLUa_kyQAEVBmlS5lybaKfJu9NDhYQo");
-    // echo "<p>" . $response->getProperties()->title . "</p>";
+    // Compose inserting range
+    $updRowsCount = count($updValues);
+    $updRange = ($tableCurRowsCount + 1) . ":" . ($tableCurRowsCount + 1 + $updRowsCount);
+
+    // Debug: tracing updatable range
+    // echo "2:" . $tableCurRowsCount . " - " .  ($tableCurRowsCount + 1) . ":" . ($tableCurRowsCount + $updRowsCount) . "<br />";
+
+    $updResponse = $sheetsService->spreadsheets_values->update("1DTdFIKfifM2pNiEZZ9dlemRNTBLumNm9KXXRZRTV9vY", "Выполнено (копия)!" . $updRange, $rangeBody, ['valueInputOption' => 'RAW']);
 
     echo "<pre>";
-    print_r($response["values"]);
+    print_r($updResponse);
     echo "</pre>";
  
 } catch (Google_Service_Exception $exception) {
  
-    // Список всех возможных ошибок и исключений https://developers.google.com/drive/v3/web/handle-errors
-    // Получаем информацию о причине возникновения исключения
-    // $reason = $exception->getErrors()[0]['reason'];
+    // Documentation: https://developers.google.com/drive/v3/web/handle-errors
+    // Example: $reason = $exception->getErrors()[0]['reason'];
 
-    echo "<pre style='background: #ff6969'>";
-    print_r($exception);
-    echo "</pre>";
- 
+    $traceFilesCount = count($exception->getTrace());
+
+    // Render error message
+    echo "<div style='background: #ff6969; padding: 15px; border: 1px solid red;'>";
+    echo "<b>[ Error ]</b> " . $exception->getErrors()[0]["message"] . "<br />";
+    echo $exception->getTrace()[$traceFilesCount-1]["file"] . " (line: " . $exception->getTrace()[$traceFilesCount-1]["line"] . ")";
+    echo "</div>";
+
+    // Debug: show all Exeption fields
+    // echo "<pre style='background: #ff6969'>";
+    // print_r($exception);
+    // echo "</pre>";
 }
